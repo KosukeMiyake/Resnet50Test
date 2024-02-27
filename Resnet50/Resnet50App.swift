d78a17a161c06e96247f8012133c60576638f5e4
KosukeMiyake <kosuke.miyake@asia-quest.jp>
2024-02-27 22:23:38 +0900

    Initial Commit

---
+++ Resnet50/Resnet50App.swift
@@ -0,0 +1,17 @@
+//
+//  Resnet50App.swift
+//  Resnet50
+//
+//  Created by kosuke.miyake on 2024/02/27.
+//
+
+import SwiftUI
+
+@main
+struct Resnet50App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
